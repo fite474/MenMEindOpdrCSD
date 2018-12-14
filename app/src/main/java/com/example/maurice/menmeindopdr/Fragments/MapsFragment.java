233f8ea0ed123dce5fe3b74beb779f2e2cef06ce
Unknown <mauricebrouwers373@hotmail.com>
2018-12-14 13:39:48 +0100

    added fragment en drawing route

--- app/src/main/java/com/example/maurice/menmeindopdr/Fragments/MapsFragment.java
+++ app/src/main/java/com/example/maurice/menmeindopdr/Fragments/MapsFragment.java
@@ -1,16 +1,33 @@
 package com.example.maurice.menmeindopdr.Fragments;
 
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.maurice.menmeindopdr.DrawingRoute.GetPathFromLocation;
+import com.example.maurice.menmeindopdr.MapsActivity;
+import com.example.maurice.menmeindopdr.NSData.Station;
+import com.example.maurice.menmeindopdr.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Queue;
-
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 /**
@@ -32,18 +49,18 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     //private List<PointOfInterest> pointOfInterests;
     List<LatLng> waypoints;
 
-//    GetPathFromLocation getPathFromLocation;
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        runnables = new LinkedBlockingQueue<>();
-//
-//        if (getArguments() != null) {
-//            routeSelected = getArguments().getInt(MapsActivity.SELECTED_ROUTE_BUNDLE_TAG);
-//        }
-//
-//        getMapAsync(this);
-//
+    GetPathFromLocation getPathFromLocation;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        runnables = new LinkedBlockingQueue<>();
+
+        if (getArguments() != null) {
+        //    routeSelected = getArguments().getInt(MapsActivity.SELECTED_ROUTE_BUNDLE_TAG);
+        }
+
+        getMapAsync(this);
+
 //        model = ViewModelProviders.of(this).get(MapsViewModel.class);
 //        switch(routeSelected){
 //            case SelectedRoute.BLIND_WALLS:
@@ -55,45 +72,45 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
 //                break;
 //
 //        }
-//    }
-//
-//    public void addMarker(final MarkerOptions marker){
-//        if(map == null){
-//            runnables.add(() -> addMarkerInternal(marker));
-//        }else{
-//            addMarkerInternal(marker);
-//        }
-//    }
-//
-//    private void addMarkerInternal(final MarkerOptions marker){
-//        map.addMarker(marker);
-//    }
-//
-//    public void addMarker(final MarkerOptions marker, final int tag){
-//        if(map == null){
-//            runnables.add(() -> addMarkerInternal(marker, tag));
-//        }else{
-//            addMarkerInternal(marker, tag);
-//        }
-//    }
-//
-//    private void addMarkerInternal(final MarkerOptions marker, final int tag){
-//        map.addMarker(marker).setTag(tag);
-//    }
-//
-//    public void addPolyLine(final PolylineOptions polyLine) {
-//        if (map == null) {
-//            runnables.add(() -> addPolyLineInternal(polyLine));
-//        } else {
-//            addPolyLineInternal(polyLine);
-//        }
-//    }
-//
-//    private void addPolyLineInternal(final PolylineOptions polyLine) {
-//        Polyline line = map.addPolyline(polyLine);
-//    }
-//
-//    public void addHistoricsToMap(List<PointOfInterest> monuments){
+    }
+
+    public void addMarker(final MarkerOptions marker){
+        if(map == null){
+            runnables.add(() -> addMarkerInternal(marker));
+        }else{
+            addMarkerInternal(marker);
+        }
+    }
+
+    private void addMarkerInternal(final MarkerOptions marker){
+        map.addMarker(marker);
+    }
+
+    public void addMarker(final MarkerOptions marker, final int tag){
+        if(map == null){
+            runnables.add(() -> addMarkerInternal(marker, tag));
+        }else{
+            addMarkerInternal(marker, tag);
+        }
+    }
+
+    private void addMarkerInternal(final MarkerOptions marker, final int tag){
+        map.addMarker(marker).setTag(tag);
+    }
+
+    public void addPolyLine(final PolylineOptions polyLine) {
+        if (map == null) {
+            runnables.add(() -> addPolyLineInternal(polyLine));
+        } else {
+            addPolyLineInternal(polyLine);
+        }
+    }
+
+    private void addPolyLineInternal(final PolylineOptions polyLine) {
+        Polyline line = map.addPolyline(polyLine);
+    }
+
+    public void addHistoricsToMap(List<Station> monuments){
 //        for(PointOfInterest poi : monuments){
 //            if(monuments.get(0) instanceof HistoricMonument) {
 //                HistoricMonument monument = (HistoricMonument) poi;
@@ -106,11 +123,11 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
 //                }
 //            }
 //        }
-//        //drawLinesToMap(monuments);
-//    }
-//
-//    public void addBlindWallsToMap(List<PointOfInterest> blindWalls){
-//
+        //drawLinesToMap(monuments);
+    }
+
+    public void addBlindWallsToMap(List<Station> blindWalls){
+
 //
 //        if(blindWalls.get(0) instanceof BlindWall) {
 //            for (PointOfInterest blindWallPoi : blindWalls) {
@@ -123,73 +140,73 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
 //            }
 //        }
 //        drawLinesToMap(blindWalls);
-//    }
-//
-//    public void drawLinesToMap(List<PointOfInterest> pointOfInterests){
-////        LocationManager locationManager = (LocationManager) App.getContext().getSystemService(Context.LOCATION_SERVICE);
-////        Criteria criteria = new Criteria();
-////        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
-////
-//        src = new LatLng(pointOfInterests.get(0).getLatitude(), pointOfInterests.get(0).getLongitude());//new LatLng(location.getLatitude(), location.getLongitude());//new LatLng(pointOfInterests.get(0).getLatitude(), pointOfInterests.get(0).getLongitude());
-//        dest = new LatLng(pointOfInterests.get(1).getLatitude(), pointOfInterests.get(1).getLongitude());
-//
-//        int targetSize = 21;
-//        List<List<PointOfInterest>> output = Lists.partition(pointOfInterests, targetSize);
-//
+    }
+
+    public void drawLinesToMap(List<Station> pointOfInterests){
+//        LocationManager locationManager = (LocationManager) App.getContext().getSystemService(Context.LOCATION_SERVICE);
+//        Criteria criteria = new Criteria();
+//        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+
+        //src = new LatLng(pointOfInterests.get(0).getLatitude(), pointOfInterests.get(0).getLongitude());//new LatLng(location.getLatitude(), location.getLongitude());//new LatLng(pointOfInterests.get(0).getLatitude(), pointOfInterests.get(0).getLongitude());
+        //dest = new LatLng(pointOfInterests.get(1).getLatitude(), pointOfInterests.get(1).getLongitude());
+
+        int targetSize = 21;
+  //      List<List<PointOfInterest>> output = Lists.partition(pointOfInterests, targetSize);
+
 //        for (List<PointOfInterest> partialList : output) {
 //            new GetPathFromLocation(src, dest, partialList, polyLine -> {
 //                addPolyLine(polyLine);
 //            }).execute();
 //        }
-//
-//    }
-//
-//
+
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-//        Log.d(TAG, "onMapReady: ");
-//        try {
-//            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(App.getContext(), R.raw.map_style_json));
-//            if (!success) {
-//                Log.e("STYLE_ERROR", "Style parsing failed.");
-//            }
-//        } catch (Resources.NotFoundException e) {
-//            Log.e("STYLE_ERROR", "Can't find style. Error: ", e);
-//        }
-//
-//        this.map = googleMap;
-//        map.setOnMarkerClickListener(this);
-//
-//        getLocationPermission();
-//
-//        LatLng[] latLngsArray = new LatLng[]{
-//                new LatLng(51.551859, 4.698705),
-//                new LatLng(51.556769, 4.843869),
-//                new LatLng(51.622466, 4.843183),
-//                new LatLng(51.634188, 4.765637),
-//                new LatLng(51.620122, 4.688058),
-//                new LatLng(51.551859, 4.698705)
-//        };
-//
-//        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-//        PolylineOptions polylineOptions = new PolylineOptions();
-//        for (LatLng latLng : latLngsArray) {
-//            builder.include(latLng);
-//            polylineOptions.add(latLng);
-//        }
-//        LatLngBounds bounds = builder.build();
-//
-//        map.addPolyline(polylineOptions);
-//        map.setLatLngBoundsForCameraTarget(bounds);
-//        map.setIndoorEnabled(false);
-//        map.getUiSettings().setZoomControlsEnabled(true);
-//        map.setMinZoomPreference(13);
-//
-//        Iterator<Runnable> iterator = runnables.iterator();
-//        while (iterator.hasNext()) {
-//            Runnable runnable = iterator.next();
-//            runnable.run();
-//        }
+        Log.d(TAG, "onMapReady: ");
+        try {
+            boolean success = true;//googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(App.getContext(), R.raw.map_style_json));
+            if (!success) {
+                Log.e("STYLE_ERROR", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("STYLE_ERROR", "Can't find style. Error: ", e);
+        }
+
+        this.map = googleMap;
+        map.setOnMarkerClickListener(this);
+
+        getLocationPermission();
+
+        LatLng[] latLngsArray = new LatLng[]{
+                new LatLng(51.551859, 4.698705),
+                new LatLng(51.556769, 4.843869),
+                new LatLng(51.622466, 4.843183),
+                new LatLng(51.634188, 4.765637),
+                new LatLng(51.620122, 4.688058),
+                new LatLng(51.551859, 4.698705)
+        };
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        PolylineOptions polylineOptions = new PolylineOptions();
+        for (LatLng latLng : latLngsArray) {
+            builder.include(latLng);
+            polylineOptions.add(latLng);
+        }
+        LatLngBounds bounds = builder.build();
+
+        map.addPolyline(polylineOptions);
+        map.setLatLngBoundsForCameraTarget(bounds);
+        map.setIndoorEnabled(false);
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.setMinZoomPreference(13);
+
+        Iterator<Runnable> iterator = runnables.iterator();
+        while (iterator.hasNext()) {
+            Runnable runnable = iterator.next();
+            runnable.run();
+        }
     }
 
     @Override
