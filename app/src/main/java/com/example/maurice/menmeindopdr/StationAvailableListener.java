104336a7a8039a9ff8863971c0f766d14f1b08a0
Marck420 <m.debont1@student.avans.nl>
2019-01-11 17:15:20 +0100

    [deleted] mapsviewModel and StationAvailableListener

--- app/src/main/java/com/example/maurice/menmeindopdr/StationAvailableListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.maurice.menmeindopdr;
-
-import java.util.List;
-//
-//public interface StationAvailableListener {
-//    void onPointOfInterestsAvailable(List<Station> pointOfInterests, RequestState state);
-//}
